28132f68f226c180eb883e2e92be0aac9431af42
Munir-talha <63232075+Munir-talha@users.noreply.github.com>
2024-11-01 11:43:26 -0700

    Minor Changes on Home Page

--- src/components/FindFlight/FindFlight.jsx
+++ src/components/FindFlight/FindFlight.jsx
@@ -1,9 +1,12 @@
+'use client'
 import React from 'react'
+import { useState } from 'react'
 import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowsTurnToDots, faArrowRight, faUser , faAngleDown , faMoneyCheckDollar } from '@fortawesome/free-solid-svg-icons';
+import { faArrowsTurnToDots, faArrowRight, faUser , faAngleDown , faMoneyCheckDollar , faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import {
     Select,
     SelectContent,
@@ -13,64 +16,122 @@ import {
     SelectTrigger,
     SelectValue,
   } from "@/components/ui/select"
-
+import './FindFlight.css'
 function FindFlight() {
+    const [selectedOption, setSelectedOption] = useState('roundTrip');
+    const handleSelect = (option) => {
+        setSelectedOption(option);
+    };
+    const FlighClass = [
+        {
+            id : 1,
+            name : 'Economy'
+        },
+        {
+            id : 2,
+            name : 'Business'
+        },
+        {
+            id : 3,
+            name : 'First Class'
+        }
+    ]
   return (
     <>
-    <div style={{width : '35rem'}} className='m-2'>
-        <p className='font-bold'>Find your Flight</p>
-        <div className='flex gap-2 '>
-            <Badge variant="secondary">
-                <FontAwesomeIcon icon={faArrowsTurnToDots} className="mr-2 w-7 h-5" /> Round trip
-            </Badge>
-            <Badge variant="success">
-                <FontAwesomeIcon icon={faArrowRight} className='mr-2 w-7 h-5'/>  One Way
-            </Badge>
-            <div>
-                <Select>
-                    <SelectTrigger className="w-[120px] flex items-center gap-2 bg-destructive text-white px-4 py-2 rounded-lg">
-                        <FontAwesomeIcon icon={faUser} className='w-5 h-5 mr-2' />
-                        <SelectValue placeholder="1" className="text-white flex-grow" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectGroup>
-                        <SelectItem value="1">1</SelectItem>
-                        <SelectItem value="2">2</SelectItem>
-                        <SelectItem value="3">3</SelectItem>
-                        <SelectItem value="4">4</SelectItem>
-                        </SelectGroup>
-                    </SelectContent>
-                </Select>
-                </div>
-                <div>
+    <div className="container mx-auto px-4">
+        <div className="grid grid-cols-12 gap-4">
+            <div className="col-span-12 lg:col-span-6 bg-white p-4 rounded-xl shadow-md">
+                <div className="col-6 m-2">
+                    <p className="font-bold">Find your Flight!</p>
+                    <div className='flex gap-2'>
+                        {/* This is roundtripBadge */}
+                        <Badge
+                            variant="secondary"
+                            onClick={() => handleSelect('roundTrip')}
+                            className={`cursor-pointer ${selectedOption === 'roundTrip' ? 'border-blue-500 shadow-lg' : 'border-gray-300'}`}
+                        >
+                            <FontAwesomeIcon icon={faArrowsTurnToDots} className="mr-2 w-7 h-5" /> Round trip
+                        </Badge>
+                        {/* This is OneWayBadge */}
+                        <Badge
+                            variant="success"
+                            onClick={() => handleSelect('oneWay')}
+                            className={`cursor-pointer ${selectedOption === 'oneWay' ? 'border-green-500 shadow-2xl' : 'border-gray-300'}`}
+                        >
+                            <FontAwesomeIcon icon={faArrowRight} className="mr-2 w-7 h-5" /> One Way
+                        </Badge>
+                        <div>
+                            <Select>
+                                <SelectTrigger className="w-auto flex items-center gap-2 bg-destructive text-white px-4 py-2 rounded-lg">
+                                    <FontAwesomeIcon icon={faUser} className='w-5 h-5 mr-2' />
+                                    <SelectValue placeholder="1" className="text-white flex-grow" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                    <SelectItem value="1">1</SelectItem>
+                                    <SelectItem value="2">2</SelectItem>
+                                    <SelectItem value="3">3</SelectItem>
+                                    <SelectItem value="4">4</SelectItem>
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                            </div>
+                            <div>
 
-                <Select>
-                    <SelectTrigger className="w-[180px] flex items-center gap-2 bg-destructive text-white px-4 py-2 rounded-lg">
-                    <FontAwesomeIcon icon={faMoneyCheckDollar} className="w-5 h-5 mr-2" />
-                    <SelectValue placeholder="Economy" className="text-white flex-grow" />
-                    </SelectTrigger>
-                    
-                    <SelectContent>
-                    <SelectGroup>
-                        <SelectItem value="economy">Economy</SelectItem>
-                        <SelectItem value="business">Business</SelectItem>
-                        <SelectItem value="first">First Class</SelectItem>
-                    </SelectGroup>
-                    </SelectContent>
-                </Select>
+                            <Select>
+                                <SelectTrigger className="w-auto flex items-center gap-2 bg-destructive text-white px-4 py-2 rounded-lg">
+                                <FontAwesomeIcon icon={faMoneyCheckDollar} className="w-5 h-5 mr-2" />
+                                <SelectValue placeholder="Economy" className="text-white flex-grow" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                <SelectGroup>
+                                {FlighClass.map((item) => (
+                                    <SelectItem key={item.id} value={item.id}>{item.name}</SelectItem>
+                                ))}
+                                </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                    </div>
+                    <div className='flex gap-3 mt-5'>
+                        <div style={{background : '#0077b6'}} className='p-8 rounded-2xl flex gap-8'>
+                            <div className="grid w-full max-w-sm items-center gap-1.5">
+                                <Label className="text-white" htmlFor="From">From</Label>
+                                <Input className="border-b-white text-white focus-visible:border-none" id="From" placeholder="From Where To Fly?" />
+                            </div>
+                            <div className="grid w-full max-w-sm items-center gap-1.5">
+                                <Label className="text-white" htmlFor="To">To</Label>
+                                <Input className="border-b-white text-white focus-visible:border-none" id="To" placeholder="What is your Destination?" />
+                            </div>
+                        </div>
+                        <div style={{background : '#0077b6'}} className='rounded-2xl h-auto'>
+                            <Button className=' rounded-2xl h-full bg-green-400'>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} className="w-7 h-7 mr-auto" />
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-span-12 lg:col-span-6 bg-gradient-to-r from-blue-500 to-blue-700 p-6 rounded-xl text-white flex items-center justify-center shadow-md">
+                <div>
+                    <h2 className="text-2xl font-bold mb-4">Explore Our Special Offers!</h2>
+                    <p className="mb-4">
+                        Check out exclusive deals on flights to top destinations. Save more on your next adventure!
+                    </p>
+                    <Button className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg">
+                        View Offers
+                    </Button>
+                </div>
             </div>
         </div>
-        <div className='bg-blue-800 p-8 rounded-2xl flex gap-8'>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label className="text-white" htmlFor="From">From</Label>
-            <Input className="border-b-white text-white focus-visible:border-none" id="From" placeholder="Select the Final Destination" />
-        </div>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-            <Label className="text-white" htmlFor="From">To</Label>
-            <Input className="border-b-white text-white focus-visible:border-none" id="From" placeholder="Select the Final Destination" />
-        </div>
-        </div>
     </div>
+
+
+
+
+    
+    
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,14 +8,21 @@ import { faBell , faGlobe , faPlaneUp } from '@fortawesome/free-solid-svg-icons'
 function Header() {
   return (
     <>
-    <div className='flex justify-between mx-7 mt-5'>
+    <div className='flex justify-between px-7 py-7 bg-gray-100 h-full'>
       <div className='flex gap-5 items-center font-bold text-c'>
         <FontAwesomeIcon icon={faPlaneUp} className='w-8' style={{ color: '#0DD496' }}/>
         <span className='text-blue-600'>E-Flight</span>
       </div>
       <div className='flex gap-5'>
-        <FontAwesomeIcon icon={faGlobe} className='w-8 text-blue-400'/>
-        <FontAwesomeIcon icon={faBell} className="w-8" />
+        
+        <Avatar>
+          <AvatarImage src="../../public/bell-solid.svg" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
         <Avatar>
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>CN</AvatarFallback>
