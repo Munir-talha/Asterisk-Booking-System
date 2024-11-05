e0fcb01f5266a43e5aa5f31e53cee39d797d3369
Munir-talha <63232075+Munir-talha@users.noreply.github.com>
2024-11-05 22:33:45 +0500

    minor adjustments

--- src/components/ExploreSection/Explore.jsx
+++ src/components/ExploreSection/Explore.jsx
@@ -6,7 +6,7 @@ import { Card, CardHeader, CardTitle, CardContent, CardDescription } from "@/com
 export default function Explore() {
   return (
     <div className="p-6 font-sans my-24 mx-7">
-      <div className="flex flex-wrap items-center justify-between mb-6 gap-4">
+      <div className="flex flex-wrap items-center justify-between gap-4 mb-16">
         <div className="flex items-center">
           <FontAwesomeIcon icon={faLocationDot} className="mr-2 text-purple-600" />
           <span className="text-3xl font-bold">
@@ -19,7 +19,7 @@ export default function Explore() {
       </div>
 
       {/* Map Section */}
-      <div className="w-full h-96 rounded-lg overflow-hidden mb-6">
+      <div className="w-full h-96 rounded-lg overflow-hidden mb-6 border-gray-300 border-2">
         <iframe
           title="map"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3457.934168978222!2d-95.36327168570266!3d29.760426481998917!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640b8b56a35c669%3A0x8b0d2b0b515e6e!2sHouston%2C%20TX%2C%20USA!5e0!3m2!1sen!2sus!4v1615738126841!5m2!1sen!2sus"
@@ -31,7 +31,7 @@ export default function Explore() {
         ></iframe>
       </div>
 
-      <div className="flex justify-evenly flex-wrap gap-4">
+      <div className="flex justify-evenly flex-wrap gap-4 my-16">
         {[1, 2, 3].map((item, index) => (
           <Card key={index} className="flex flex-col w-96 p-4 rounded-lg shadow-md bg-white">
             <div className="flex-none w-full h-32 bg-cover bg-center rounded-md mb-4"
