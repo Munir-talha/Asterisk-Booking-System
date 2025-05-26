fa5ca5b4bac3a77eb4ac334cf537c2ce3e48a1a8
Zeshan <100864726+Zeshanbutt1431@users.noreply.github.com>
2025-05-26 21:36:07 +0500

    Update next.config.mjs
    Update config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true, // Ensures all pages are treated as static files with .html
+  serverActions: true, // ✅ OK if you're using Server Actions
+
 };
 
 export default nextConfig;
